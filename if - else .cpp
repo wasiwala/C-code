7b8cf7bafb45430b1cc12feea4ec24b112413a73
wala481652 <50227444+wala481652@users.noreply.github.com>
2019-05-06 14:30:48 +0800

    Update if - else .cpp

--- if - else .cpp
+++ if - else .cpp
@@ -1,3 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main
+int main(void)
+{
+  int i,j;
+  i=1;
+  printf("結果:");
+  if(j<10)
+  {
+    j=i+1;
+    printf("結果:%d",j);
+  }
+  else
+  {
+    printf("j=%d",j);
+  }
+  
+}
